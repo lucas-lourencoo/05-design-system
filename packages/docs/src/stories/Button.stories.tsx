ebce817a600d4288c6c4ffa9f5d543b4f893e521
Lucas Lourenço <lucascelestiano@gmail.com>
2023-07-03 19:30:39 -0400

    ci: Add TurboRepo cache on deploy build

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -1,5 +1,5 @@
 import { StoryObj, Meta } from "@storybook/react";
-import { Button, ButtonProps } from "@ignite-ui/react";
+import { Button, ButtonProps } from "@nego-tech/react";
 import { ArrowRight } from "phosphor-react";
 
 export default {
